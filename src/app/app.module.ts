28b41b490aa1817fd4ffd8e7b85e639ee655f621
Ana Bennett <anabennett1111@gmail.com>
2022-10-17 19:03:25 -0500

    Add styling to form and jammer profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { ResultsPageComponent } from './results-page/results-page.component';
+import { JammerProfileComponent } from './jammer-profile/jammer-profile.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchbarComponent,
+    ResultsPageComponent,
+    JammerProfileComponent,
   ],
   imports: [
     BrowserModule,
