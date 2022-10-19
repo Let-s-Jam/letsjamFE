d35e82066ce447024acfe02c6fd47dfe83a21064
Maya Kappen <mayakappen@gmail.com>
2022-10-19 13:51:03 -0600

    Add imports to User Profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { JammerProfileComponent } from '../jammer-profile/jammer-profile.component';
+import { AcceptedJammersComponent } from '../accepted-jammers/accepted-jammers.component';
+import { JamRequestsComponent } from '../jam-requests/jam-requests.component';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
