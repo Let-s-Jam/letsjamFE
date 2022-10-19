168ef52a0d6a720206fa2f6649dc00a5eeacba90
emmacintosh <emmammrussell@gmail.com>
2022-10-19 15:20:35 -0500

    Add dummy test file and imports for local testing before using dummy server

--- src/app/jammer-profile/jammer-profile.component.ts
+++ src/app/jammer-profile/jammer-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FOUNDJAMMERS } from 'src/dummy-data';
 
 @Component({
   selector: 'app-jammer-profile',
---
+++ src/dummy-data.ts
@@ -0,0 +1,46 @@
+export interface foundJammers {
+    data: object
+}
+
+export const FOUNDJAMMERS: foundJammers = {
+    "data": [
+   {
+     "id": "1",
+     "type": "user",
+     "results": {
+  
+      "found_users": [
+           {
+            "name": "Dominic Torretto",
+            "about": "I live my life a quarter mile at a time",
+            "distance": "4.7",
+            "picture_url": "https://i.pinimg.com/736x/e4/b0/43/e4b043d9231bd9767dc4d5ec0e7af3e7.jpg",
+            "played_instruments": [
+                  "steel drum",
+                  "guitar"
+              ],
+            "sought_instruments": [
+                  "bass",
+                  "cello"
+              ]           
+           },
+           {
+            "name": "Bob Saget Jr.",
+            "about": "I miss my dad",
+            "distance": "11.2",
+            "picture_url": "https://i.pinimg.com/736x/e4/b0/43/e4b043d9231bd9767dc4d5ec0e7af3e7.jpg",
+            "played_instruments": [
+                  "harmonica",
+                  "guitar"
+              ],
+            "sought_instruments": [
+                  "bass",
+                  "guitar",
+                  "piano"
+              ]           
+           }
+       ],
+     }
+   }
+    ]
+  }
