20ffa99820d02f89e3b12b75c37cee39fbca885d
emmacintosh <emmammrussell@gmail.com>
2022-10-19 20:02:23 -0500

    Add user service and injectable for retrieving search data from mock API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,7 +23,9 @@ import { AcceptedJammersComponent } from './accepted-jammers/accepted-jammers.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    //injecting an instance of the http client, components can inject that if they want
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/jammer-profile/jammer-profile.component.ts
+++ src/app/jammer-profile/jammer-profile.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { FOUNDJAMMERS } from 'src/dummy-data';
 import { foundJammers } from 'src/dummy-data';
+import { UserService } from '../services/user-service';
 
 @Component({
   selector: 'app-jammer-profile',
   templateUrl: './jammer-profile.component.html',
-  styleUrls: ['./jammer-profile.component.css']
+  styleUrls: ['./jammer-profile.component.css'],
+  providers: [UserService]
+  //how we're telling the component that userservice should be created directly from the class
 })
 export class JammerProfileComponent implements OnInit {
 
-  constructor() { }
+public founds: any 
 
-  founds = FOUNDJAMMERS.data[0].results.found_users;
+  constructor(private userService: UserService) { 
+    //this adds userService obviously lol
+  }
+
+  // founds = FOUNDJAMMERS.data[0].results.found_users;
 
   ngOnInit(): void {
-    // FOUNDJAMMERS.data = found;
+    this.userService.getJammers().subscribe((data: any) => {
+      console.log(data)
+      this.founds = data.data[0].results.found_users
+    })
   }
+
+  // data is the repsonse from the server
+
+  //observables
 
   //found: foundJammers
 
 }
+
+//use a service to inject into the component
+//service encapsulates any communication w the server
+//could do a single service with everything in it, or multiple services if you're using different endpoints
