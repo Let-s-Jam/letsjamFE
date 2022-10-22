10b31cb11d3c14e8ebcac356f3233373cc60edd5
Ana Bennett <anabennett1111@gmail.com>
2022-10-22 12:05:14 -0500

    Error handling with allowing the user to edit their profile

--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -16,11 +16,38 @@ export class UserService {
   getIncomingJammerProfiles() {
     return this.http.get('https://1d9a3efd-bdd5-4feb-baad-39d400f80776.mock.pstmn.io/api/v1/user/1/connections');
   }
-  
+  editProfile(value: any) {
+   this.http.patch('https://1d9a3efd-bdd5-4feb-baad-39d400f80776.mock.pstmn.io/api/v1/user/1', {
+      "attributes": value
+   })
+    //  .then(response => console.log(response))
+  }
+    
+}
+// userEdit$ = new Observable(observer => {
+//   fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+//     .then(response => response.json())
+//     .then(userEdit => {
+//       observer.next(userEdit);
+//       observer.complete();
+//     })
+//     .catch(err => observer.error(err))
+// });
+// pikachu$ = new Observable(observer => {
+//   fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+//     .then(response => response.json())
+//     .then(pikachu => {
+//       observer.next(pikachu);
+//       observer.complete();
+//     })
+//     .catch(err => observer.error(err))
+// });
+
+
+
 
   //add another method like this for each request, after they fix the json we can remove the responsetype argument
   //in your component once you get data thats well structured 
 
   //understand dependency injections
   //understand how the userservice is getting injected
-}
--- src/app/user-edit/user-edit.component.ts
+++ src/app/user-edit/user-edit.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UserService } from '../services/user-service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.css']
 })
+
 export class UserEditComponent implements OnInit {
+   
+   userEdit = ({
+    name: "",
+    display_email: "",
+    zipcode: "",
+    about: "",
+    genre: [],
+    played_instruments: []
+  })
 
   @Output() updateEdit = new EventEmitter<boolean>();
-  constructor() { 
+  constructor(private userService: UserService) { 
    }
 
   ngOnInit(): void {
@@ -16,6 +27,11 @@ export class UserEditComponent implements OnInit {
  
   submit(): void {
     this.updateEdit.emit(false)
+    console.log(this.userEdit.name, "name")
+    this.userService.editProfile(this.userEdit)
+    
+
    }
+   
 
 }
