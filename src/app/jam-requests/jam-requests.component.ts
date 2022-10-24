e9c2ed7a87ea7c18018a83b199989667af1507b6
emmacintosh <emmammrussell@gmail.com>
2022-10-24 14:34:40 -0500

    Add Typescript interface for for jam requests component

--- src/app/jam-requests/jam-requests.component.ts
+++ src/app/jam-requests/jam-requests.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user-service';
 
+interface Requests {
+  id: number,
+  name: string,
+  about: string,
+  zipcode: number,
+  picture_url: string,
+  instruments: object[],
+  needs_instruments: object[],
+  genres: object[]
+}
+
 @Component({
   selector: 'app-jam-requests',
   templateUrl: './jam-requests.component.html',
@@ -9,14 +20,23 @@ import { UserService } from '../services/user-service';
 })
 export class JamRequestsComponent implements OnInit {
 
-  public requests: any
+  public requests: Requests[] = [{
+    id: 0,
+    name:"",
+    about: "",
+    zipcode: 0,
+    picture_url: "",
+    instruments: [],
+    needs_instruments: [],
+    genres: [],
+  }];
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.userService.getIncomingJammerProfiles().subscribe((data: any) => {
       console.log('requests', data)
-      this.requests = data.data.attributes.connections_pending
+      this.requests = data.data.attributes.requests_pending
     })
   }
 
