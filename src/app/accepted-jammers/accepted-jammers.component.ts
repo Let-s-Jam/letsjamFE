bb4ae6048d7cb69762921f4766232380c762f035
Ana Bennett <anabennett1111@gmail.com>
2022-10-24 14:16:04 -0500

    Add a friend interface to the accepted jammers componenent

--- src/app/accepted-jammers/accepted-jammers.component.ts
+++ src/app/accepted-jammers/accepted-jammers.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user-service';
 
+
+interface Friend {
+  id: number,
+  name: string,
+  display_email: string,
+  about: string,
+  zipcode: number,
+  picture_url: string,
+  instruments: object[],
+  needs_instruments: object[],
+  genres: object[]
+}
+
 @Component({
   selector: 'app-accepted-jammers',
   templateUrl: './accepted-jammers.component.html',
@@ -9,7 +22,19 @@ import { UserService } from '../services/user-service';
 })
 export class AcceptedJammersComponent implements OnInit {
 
-  public friends: any
+  public friends: Friend[] = [{
+    id: 0,
+    name:"",
+    display_email: "",
+    about: "",
+    zipcode: 0,
+    picture_url: "",
+    instruments: [],
+    needs_instruments: [],
+    genres: [],
+
+  }];
+  
 
   constructor(private userService: UserService) { }
 
@@ -21,3 +46,5 @@ export class AcceptedJammersComponent implements OnInit {
   }
 
 }
+
+
