f3ef6fb239281b4ee65d161c682789964114e310
Ana Bennett <anabennett1111@gmail.com>
2022-10-25 15:09:31 -0500

    Captured search result values in a variable so we could send them in our GET request for the future

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -15,7 +15,7 @@ public searchResultGenre: any = ""
 public searchResultDistance: any = ""
 public searchResultValues: String[] = []
 
-@Output() valuesToEmit = new EventEmitter<Array<String>>();
+@Output() valuesToEmit = new EventEmitter<String>();
 
   constructor() { 
   }
@@ -32,12 +32,14 @@ public searchResultValues: String[] = []
     if (this.searchResultInstrument){this.searchResultValues.push(`instrument=${this.searchResultInstrument}`)}
     if (this.searchResultGenre){this.searchResultValues.push(`genre=${this.searchResultGenre}`)}
     if (this.searchResultDistance){this.searchResultValues.push(`distance=${this.searchResultDistance}`)}
-    this.valuesToEmit.emit(this.searchResultValues)
+    const valuesToSend:string = this.searchResultValues.join('&')
+    this.valuesToEmit.emit(valuesToSend)
     console.log(this.searchResultName)
     console.log(this.searchResultInstrument)
     console.log(this.searchResultGenre)
     console.log(this.searchResultDistance)
     console.log(this.searchResultValues)
+    console.log(valuesToSend) //)
   }
 
 }
